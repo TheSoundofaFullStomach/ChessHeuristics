e4e5c310cff81715473a3cd0d7bb6b27fa6ad834
TheSoundofaFullStomach <106417339+TheSoundofaFullStomach@users.noreply.github.com>
2023-03-05 10:25:23 -1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import learn as learn
+import chess
+import numpy
+import pandas
+
+import Board
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_start():
+    # Use a breakpoint in the code line below to debug your script.
+    print("Starting Chess Heuristics")  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_start()
+    # Board.init_board()
